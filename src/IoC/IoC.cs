8649d7df10f71576671305918902d7f81908806e
LuiggiCV <luiggicuevas92@gmail.com>
2022-06-05 15:07:05 -0300

    Add IoC and unit tests

---
+++ src/IoC/IoC.cs
@@ -0,0 +1,31 @@
+using Services;
+using Controllers;
+namespace IoC;
+
+public class Ioc
+{
+    private static Ioc? _instance;
+    private static Dictionary<string, object> _iocInstanceDict = new Dictionary<string, object>();
+    
+    private Ioc()
+    {
+        _iocInstanceDict.Add("ProductRepository", new ProductRepository());
+        _iocInstanceDict.Add("ProductController", new ProductController(
+                            (ProductRepository)_iocInstanceDict["ProductRepository"]));
+    }
+
+    public static object Get(string key)
+    {
+        return _iocInstanceDict.TryGetValue(key, out object? outObj) ? outObj : throw new KeyNotFoundException($"Service was not found with key {key}.");
+    }
+
+    public static Ioc GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Ioc();
+        }
+
+        return _instance;
+    }
+}
