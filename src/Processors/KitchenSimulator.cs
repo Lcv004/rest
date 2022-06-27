e95f7684280ead5804b8154d1bdc66cc676388c7
LuiggiCV <luiggicuevas92@gmail.com>
2022-06-27 01:18:39 -0300

    Testing KS thread

---
+++ src/Processors/KitchenSimulator.cs
@@ -0,0 +1,30 @@
+﻿namespace Processors;
+
+public class KitchenSimulator
+{
+    public static async Task StartAsync()
+    {
+        Console.WriteLine($"-->KS: Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        Task<bool> prepareOrder1 = PrepareOrder(5000);
+        Task<bool> prepareOrder2 = PrepareOrder(2000);
+        Task<bool> prepareOrder3 = PrepareOrder(7000);
+        List<Task<bool>> prepareOrderList = new List<Task<bool>>{prepareOrder1, prepareOrder2, prepareOrder3};
+        await Task.WhenAll(prepareOrderList);        
+        Console.WriteLine("-->KS: Order ready!");
+    }
+
+    public static async Task<bool> PrepareOrder(int preparationTime)
+    {
+        Console.WriteLine($"---->Task: Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        var task = Task<bool>.Run(()=>
+        {
+            Console.WriteLine("---->Task: Doing some work...");
+            Thread.Sleep(preparationTime);
+            Console.WriteLine("---->Task: Work finished!");
+            return true;
+        });
+
+        bool result = await task;
+        return result;
+    }
+}
---
+++ src/Processors/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Processors;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Main: Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        Task kitchenTask = KitchenSimulator.StartAsync();
+        bool flag = true;
+        
+        while (flag)
+        {
+            if(kitchenTask.IsCompleted)
+            {
+                Console.WriteLine("Completed!");
+                flag = false;
+            }
+        }
+        Console.ReadLine();
+    }
+}
