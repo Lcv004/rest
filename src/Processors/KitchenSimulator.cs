c3f5f91f24ea9622af1a4364989de5d01dfec4b7
LuiggiCV <luiggicuevas92@gmail.com>
2023-01-24 22:25:07 -0400

    Add events to processor class

--- src/Processors/KitchenSimulator.cs
+++ src/Processors/KitchenSimulator.cs
@@ -7,4 +7,39 @@ public class KitchenSimulator : Processor
         System.Console.WriteLine("~~> ¡Hola! son las {0}.", DateTime.Now.TimeOfDay);
         Thread.Sleep(interval);
     }
+
+    public override void OnCreate()
+    {
+        // Not implemented
+    }
+
+    public override void OnStart()
+    {
+        // Not implemented
+    }
+
+    public override void BeforeProcess()
+    {
+        // Not implemented
+    }
+
+    public override void AfterProcess()
+    {
+        // Not implemented
+    }
+
+    public override void OnPause()
+    {
+        // Not implemented
+    }
+
+    public override void OnResume()
+    {
+        // Not implemented
+    }
+
+    public override void OnStop()
+    {
+        // Not implemented
+    }
 }
--- src/Processors/Processor.cs
+++ src/Processors/Processor.cs
@@ -3,19 +3,52 @@
 public abstract class Processor : BaseThread
 {
     private bool _isPaused = false;
+    private bool _onPauseOnce = false;
+    private bool _onResumeOnce = false;
     private bool _stopRequested = false;
     private int _interval = 500;
 
+    protected Processor()
+    {
+        OnCreate();
+    }
+
+    public abstract void OnCreate();
+    public abstract void OnStart();
+    public abstract void OnStop();
+    public abstract void OnPause();
+    public abstract void OnResume();
+    public abstract void BeforeProcess();
     public abstract void Process(int interval);
+    public abstract void AfterProcess(); 
     public override void RunThread()
     {
+        OnStart();
+
         while (!_stopRequested)
         {
             if (!_isPaused)
             {
+                if (!_onResumeOnce && _onPauseOnce)
+                {
+                    OnResume();
+                }
+                _onPauseOnce = false;
+
                 Process(_interval);
             }
+            else
+            {
+                if (!_onPauseOnce)
+                {
+                    OnPause();
+                    _onPauseOnce = true;
+                }
+                _onResumeOnce = false;
+            }
         }
+
+        OnStop();
     }
 
     public void PauseRequest() => _isPaused = true;
